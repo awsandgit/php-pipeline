180e5a1292b82126ef7d0aa82791adbd4946a797
awsandgit <aws4em@gmail.com>
2023-05-23 16:27:23 +0530

    clean up

--- scripts/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -x
-mkdir -p build
-cp -fr index.js package-lock.json ecosystem.config.js scripts/startnode.sh build/
--- scripts/startnode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-set -x
-
-sudo /usr/bin/pm2 reload index.js 2> /dev/null || sudo /usr/bin/pm2 start index.js 
